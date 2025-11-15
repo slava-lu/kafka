75f31dc893a09a81a76926d1e541b829bb1d4619
slavalu <slavalu74@gmail.com>
2025-11-15 18:40:56 +0100

    flyway validation

--- src/main/java/com/example/demo/config/FlywayConfig.java
+++ src/main/java/com/example/demo/config/FlywayConfig.java
@@ -4,9 +4,11 @@ import org.flywaydb.core.api.FlywayException;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Slf4j
 @Configuration
+@Profile("local")
 public class FlywayConfig {
 
     @Bean
@@ -14,11 +16,15 @@ public class FlywayConfig {
         return flyway -> {
             try {
                 flyway.validate();
+                flyway.migrate();
+
+                log.info("Flyway validate -> migrate succeeded.");
             } catch(FlywayException ex) {
-                log.info("Flyway clean");
                 flyway.clean();
                 flyway.migrate();
 
+                log.error("Flyway validation failed: {}", ex.getMessage());
+                log.info("Flyway clean completed.");
             }
         };
     }
