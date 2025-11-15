9849c3516c39b24dc90d99baec5bdde42422d25f
slavalu <slavalu74@gmail.com>
2025-11-15 17:29:56 +0100

    cleanup

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
