bdfdbf069bc1335406a623e0f759333bb4e2e50e
Viacheslav Lushchinskiy <viacheslav.lushchinskiy@bit.admin.ch>
2025-11-16 11:49:37 +0100

    the code fixed

--- src/main/java/com/example/demo/controller/EchoController.java
+++ src/main/java/com/example/demo/controller/EchoController.java
@@ -1,14 +1,23 @@
 package com.example.demo.controller;
 
 import com.example.api.EchoApi;
+import com.example.model.EchoRequest;
+import com.example.model.EchoResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Component
 public class EchoController implements EchoApi {
 
     @Override
-    public ResponseEntity<String> sendString(String body) {
-        return ResponseEntity.ok(body);
+    public ResponseEntity<EchoResponse> sendString(EchoRequest body) {
+
+        EchoResponse response = new EchoResponse();
+
+        response.setId(body.getId());
+        response.setMessage(body.getMessage());
+
+        return ResponseEntity.ok(response);
     }
 }
